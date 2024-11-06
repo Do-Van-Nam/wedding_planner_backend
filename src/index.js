06be2e442e170a81b0c8a49d2706dd9eb2ff06d9
Nam Đỗ Văn <nam15sep2003@gmail.com>
2024-11-06 17:21:17 +0700

    fix bugs 9

--- src/index.js
+++ src/index.js
@@ -34,13 +34,18 @@ const port = process.env.PORT
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ['https://wedding-planner-frontend.up.railway.app/','http://localhost:3000', 'https://wedding-planner-frontend-url.railway.app'],
-    credentials:true
+    origin: ['https://wedding-planner-frontend.up.railway.app','http://localhost:3000', 'https://wedding-planner-frontend-url.railway.app'],
+    credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Chấp nhận các phương thức yêu cầu
+  allowedHeaders: ['Content-Type', 'Authorization'] // Đảm bảo các header cần thiết được phép sử dụng
+
 }))
 // {
 //     origin: 'http://localhost:3000',
 //     credentials: true
 // }
+// Handle preflight OPTIONS requests
+app.options('*', cors()); // Tạo phản hồi cho các yêu cầu OPTIONS từ frontend
 
 const route = require('./routes')
 route(app)
